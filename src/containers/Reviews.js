af56bd25351e913072c66d8a8375045826b8c15a
Mat Warger <mwarger@gmail.com>
2018-07-21 14:03:38 -0500

    refactored to Query component

--- src/containers/Reviews.js
+++ src/containers/Reviews.js
@@ -1,48 +1,56 @@
+// @flow
 import React from 'react'
 import Amplify, { graphqlOperation, Auth } from 'aws-amplify'
 import { Connect } from 'aws-amplify-react'
 import { ListGroup, ListGroupItem } from 'react-bootstrap'
 import NewReview from './NewReview'
 
-const ListReviews = `query ListReviews($gameId: ID!) {
-  queryReviewsByGameId(gameId: $gameId){
-    items {
-      id
-      author
-      rating
-      gameId
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const ListReviews = gql`
+  query ListReviews($gameId: ID!) {
+    reviews: queryReviewsByGameId(gameId: $gameId) {
+      items {
+        id
+        author
+        rating
+        gameId
+      }
     }
   }
-}`
+`
 
-export default ({ userId, gameId }) => {
-  console.log('userId', userId)
-  console.log('gameId', gameId)
+type ReviewArgs = {
+  userId: string,
+  gameId: string
+}
+
+export default ({ userId, gameId }: ReviewArgs) => {
   if (!userId || !gameId) return null
 
   return (
-    <React.Fragment>
-      <Connect query={graphqlOperation(ListReviews, { gameId })}>
-        {({ data }) => {
-          // console.log('data', data)
-          if (!data || !data.queryReviewsByGameId) return null
+    <Query query={ListReviews} variables={{ userId, gameId }}>
+      {({ data, loading, error }) => {
+        if (loading) return <div>Loading...</div>
 
-          const { queryReviewsByGameId } = data
+        if (error) return <div>Something bad happened!</div>
 
-          if (queryReviewsByGameId && queryReviewsByGameId.items.length > 0) {
-            return (
-              <ListGroup>
-                <h4>Reviews</h4>
-                {queryReviewsByGameId.items.map(item => (
-                  <ListGroupItem key={item.id}>
-                    {item.author || 'Anonymous'} - {item.rating}
-                  </ListGroupItem>
-                ))}
-              </ListGroup>
-            )
-          }
-        }}
-      </Connect>
-    </React.Fragment>
+        if (!data || !data.queryReviewsByGameId) return null
+        const { queryReviewsByGameId } = data
+        return queryReviewsByGameId && queryReviewsByGameId.items.length ? (
+          <ListGroup>
+            <h4>Reviews</h4>
+            {queryReviewsByGameId.items.map(item => (
+              <ListGroupItem key={item.id}>
+                {item.author || 'Anonymous'} - {item.rating}
+              </ListGroupItem>
+            ))}
+          </ListGroup>
+        ) : (
+          <p>No reviews to show!</p>
+        )
+      }}
+    </Query>
   )
 }
